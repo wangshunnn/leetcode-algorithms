b2d4e2980819789345202726dbc287f01f218ba4
Wang Shun <103867940+wangshunnn@users.noreply.github.com>
2022-05-22 10:06:27 +0800

    Update 2.Add_Two_Numbers.ts

--- 2.Add_Two_Numbers.ts
+++ 2.Add_Two_Numbers.ts
@@ -9,3 +9,26 @@
  *     }
  * }
  */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  const sumList = new ListNode();
+  let findLastSumList = sumList;
+  let lastJinwei = 0;
+  while (l1 || l2 || lastJinwei) {
+    let val = 0;
+    if (l1) {
+      val += l1.val;
+      l1 = l1.next;
+    }
+    if (l2) {
+      val += l2.val;
+      l2 = l2.next;
+    }
+    findLastSumList.val = (val + lastJinwei) % 10;
+    lastJinwei = Math.floor((val + lastJinwei) / 10);
+    if (!l1 && !l2 && !lastJinwei) break;
+    findLastSumList.next = new ListNode();
+    findLastSumList = findLastSumList.next;
+  }
+  return sumList;
+};
