cb04dbc6768aefd3f0c4429e60d12c467c7ca478
Wang Shun <103867940+wangshunnn@users.noreply.github.com>
2022-07-31 11:48:25 +0800

    Create 11.Container_With_Most_Water.ts

---
+++ 11.Container_With_Most_Water.ts
@@ -0,0 +1,19 @@
+/**
+ * 双指针。空间:O(1),时间O(N),只遍历一次。
+ */
+function maxArea(height: number[]): number {
+    // 左右指针
+    let left: number = 0;
+    let right: number = height.length - 1;
+    let volum: number = 0;
+    while (left < right) {
+        const _volum: number = (right - left) * Math.min(height[left], height[right])
+        volum = Math.max(volum, _volum)
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return volum
+};
