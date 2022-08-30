62363848b8f76fbe8621702e94b645ac53c495b6
Wang Shun <103867940+wangshunnn@users.noreply.github.com>
2022-08-31 00:19:47 +0800

    Update and rename 15.3Sum.ts to 015.3Sum.ts

--- 015.3Sum.ts
+++ 015.3Sum.ts
@@ -3,34 +3,34 @@
  * 空间O(N), 时间O(N2)
  */
 function threeSum(nums: number[]): number[][] {
-    nums.sort((a, b) => a - b);
-    let result: number[][] = [];
+    nums.sort((a, b) => a - b)
+    let result: number[][] = []
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] > 0) {
-            break;
+            break
         }
         if (nums[i] === nums[i - 1]) {
             // 防止重复
-            continue;
+            continue
         }
-        let left = i + 1;
-        let right = nums.length - 1;
+        let left = i + 1
+        let right = nums.length - 1
         while (left < right) {
             // 双指针
-            const sum = nums[i] + nums[left] + nums[right];
+            const sum = nums[i] + nums[left] + nums[right]
             if (sum < 0) {
-                left++;
+                left++
             } else if (sum === 0) {
-                result.push([nums[i], nums[left], nums[right]]);
+                result.push([nums[i], nums[left], nums[right]])
                 while (nums[left + 1] === nums[left]) {
                     // 防止重复
-                    left++;
+                    left++
                 }
-                left++;
+                left++
             } else {
-                right--;
+                right--
             }
         }
     }
     return result
-};
+}
